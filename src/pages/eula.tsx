b2e5bf1564245b58cb32e66282f984b3934486db
Tyler Norbury <tylernorbury96@gmail.com>
2020-11-17 11:54:24 -0800

    404 and eula page to typescript

--- src/pages/eula.tsx
+++ src/pages/eula.tsx
@@ -14,6 +14,15 @@ const EulaWrapper = styled.div`
     padding-left: 1rem;
     padding-right: 1rem;
   }
+
+  .company_name {
+    font-weight: bold;
+    font-style: italic;
+  }
+  .app_name {
+    font-weight: bold;
+    font-style: italic;
+  }
 `
 export default function Eula() {
   return (
@@ -24,21 +33,23 @@ export default function Eula() {
       <EulaWrapper>
         <h2>
           End-User License Agreement (EULA) of{" "}
-          <span class="app_name">Music Note</span>
+          <span className="app_name">Music Note</span>
         </h2>
         <p>
           This End-User License Agreement ("EULA") is a legal agreement between
-          you and <span class="company_name">Tyler Norbury</span>. Our EULA was
-          created by <a href="https://www.eulatemplate.com">EULA Template</a>{" "}
-          for <span class="app_name">Music Note</span>.
+          you and <span className="company_name">Tyler Norbury</span>. Our EULA
+          was created by{" "}
+          <a href="https://www.eulatemplate.com">EULA Template</a> for{" "}
+          <span className="app_name">Music Note</span>.
         </p>
         <p>
           This EULA agreement governs your acquisition and use of our{" "}
-          <span class="app_name">Music Note</span> software ("Software")
-          directly from <span class="company_name">Tyler Norbury</span> or
-          indirectly through a <span class="company_name">Tyler Norbury</span>{" "}
-          authorized reseller or distributor (a "Reseller"). Our Privacy Policy
-          was created by{" "}
+          <span className="app_name">Music Note</span> software ("Software")
+          directly from <span className="company_name">Tyler Norbury</span> or
+          indirectly through a{" "}
+          <span className="company_name">Tyler Norbury</span> authorized
+          reseller or distributor (a "Reseller"). Our Privacy Policy was created
+          by{" "}
           <a href="https://www.generateprivacypolicy.com/">
             the Privacy Policy Generator
           </a>
@@ -47,15 +58,16 @@ export default function Eula() {
         <p>
           Please read this EULA agreement carefully before completing the
           installation process and using the{" "}
-          <span class="app_name">Music Note</span> software. It provides a
-          license to use the <span class="app_name">Music Note</span> software
-          and contains warranty information and liability disclaimers.
+          <span className="app_name">Music Note</span> software. It provides a
+          license to use the <span className="app_name">Music Note</span>{" "}
+          software and contains warranty information and liability disclaimers.
         </p>
         <p>
           If you register for a free trial of the{" "}
-          <span class="app_name">Music Note</span> software, this EULA agreement
-          will also govern that trial. By clicking "accept" or installing and/or
-          using the <span class="app_name">Music Note</span> software, you are
+          <span className="app_name">Music Note</span> software, this EULA
+          agreement will also govern that trial. By clicking "accept" or
+          installing and/or using the{" "}
+          <span className="app_name">Music Note</span> software, you are
           confirming your acceptance of the Software and agreeing to become
           bound by the terms of this EULA agreement.
         </p>
@@ -69,28 +81,29 @@ export default function Eula() {
         </p>
         <p>
           This EULA agreement shall apply only to the Software supplied by{" "}
-          <span class="company_name">Tyler Norbury</span> herewith regardless of
-          whether other software is referred to or described herein. The terms
-          also apply to any <span class="company_name">Tyler Norbury</span>{" "}
-          updates, supplements, Internet-based services, and support services
-          for the Software, unless other terms accompany those items on
-          delivery. If so, those terms apply.
+          <span className="company_name">Tyler Norbury</span> herewith
+          regardless of whether other software is referred to or described
+          herein. The terms also apply to any{" "}
+          <span className="company_name">Tyler Norbury</span> updates,
+          supplements, Internet-based services, and support services for the
+          Software, unless other terms accompany those items on delivery. If so,
+          those terms apply.
         </p>
         <h3>License Grant</h3>
 
         <p>
-          <span class="company_name">Tyler Norbury</span> hereby grants you a
-          personal, non-transferable, non-exclusive licence to use the{" "}
-          <span class="app_name">Music Note</span> software on your devices in
-          accordance with the terms of this EULA agreement.
+          <span className="company_name">Tyler Norbury</span> hereby grants you
+          a personal, non-transferable, non-exclusive licence to use the{" "}
+          <span className="app_name">Music Note</span> software on your devices
+          in accordance with the terms of this EULA agreement.
         </p>
 
         <p>
-          You are permitted to load the <span class="app_name">Music Note</span>{" "}
-          software (for example a PC, laptop, mobile or tablet) under your
-          control. You are responsible for ensuring your device meets the
-          minimum requirements of the <span class="app_name">Music Note</span>{" "}
-          software.
+          You are permitted to load the{" "}
+          <span className="app_name">Music Note</span> software (for example a
+          PC, laptop, mobile or tablet) under your control. You are responsible
+          for ensuring your device meets the minimum requirements of the{" "}
+          <span className="app_name">Music Note</span> software.
         </p>
 
         <p>You are not permitted to:</p>
@@ -117,7 +130,7 @@ export default function Eula() {
           </li>
           <li>
             use the Software for any purpose that{" "}
-            <span class="company_name">Tyler Norbury</span> considers is a
+            <span className="company_name">Tyler Norbury</span> considers is a
             breach of this EULA agreement
           </li>
         </ul>
@@ -125,18 +138,18 @@ export default function Eula() {
         <h3>Intellectual Property and Ownership</h3>
 
         <p>
-          <span class="company_name">Tyler Norbury</span> shall at all times
+          <span className="company_name">Tyler Norbury</span> shall at all times
           retain ownership of the Software as originally downloaded by you and
           all subsequent downloads of the Software by you. The Software (and the
           copyright, and other intellectual property rights of whatever nature
           in the Software, including any modifications made thereto) are and
           shall remain the property of{" "}
-          <span class="company_name">Tyler Norbury</span>.
+          <span className="company_name">Tyler Norbury</span>.
         </p>
 
         <p>
-          <span class="company_name">Tyler Norbury</span> reserves the right to
-          grant licences to use the Software to third parties.
+          <span className="company_name">Tyler Norbury</span> reserves the right
+          to grant licences to use the Software to third parties.
         </p>
 
         <h3>Termination</h3>
@@ -145,7 +158,7 @@ export default function Eula() {
           This EULA agreement is effective from the date you first use the
           Software and shall continue until terminated. You may terminate it at
           any time upon written notice to{" "}
-          <span class="company_name">Tyler Norbury</span>.
+          <span className="company_name">Tyler Norbury</span>.
         </p>
 
         <p>
@@ -162,7 +175,7 @@ export default function Eula() {
         <p>
           This EULA agreement, and any dispute arising out of or in connection
           with this EULA agreement, shall be governed by and construed in
-          accordance with the laws of <span class="country">us</span>.
+          accordance with the laws of <span className="country">us</span>.
         </p>
       </EulaWrapper>
     </div>
